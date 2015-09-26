82a07389b9df2896bf604a0eb7b1984256f83962
Hayden Smith <darthideut@gmail.com>
2015-09-25 23:44:01 -0500

    Shenanigans! I'm calling it!

--- debian-software-updater.py
+++ debian-software-updater.py
@@ -3,4 +3,4 @@ import os
 print("Beginning the update process")
 os.system ("sudo apt-get update")
 os.system ("sudo apt-get dist-upgrade")
-print("All finished, your computer is now ready to roll!")
+print("All finished, your computer is now ready to roll! Taco Bell is da bomb!")

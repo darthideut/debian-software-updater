f8fc6b7f3389e030ce2821e898c96849aeed4dc9
Hayden Smith <darthideut@gmail.com>
2015-09-25 23:37:17 -0500

    Added beginning message

--- debian-software-updater.py
+++ debian-software-updater.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+print("Beginning the update process")
 os.system ("sudo apt-get update")
 os.system ("sudo apt-get dist-upgrade")
 print("All done, your system is now up to date!")

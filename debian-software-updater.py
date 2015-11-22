39be923763f50d5df2b915b663d92d6b9426e2b1
Hayden Smith <hayden@hayden-laptop-debian>
2015-11-21 18:36:10 -0600

    Taco Bell is still da bomb, but not relevent to program. :(

--- debian-software-updater.py
+++ debian-software-updater.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 import os
 print("Beginning the update process")
-os.system ("sudo apt-get update")
-os.system ("sudo apt-get dist-upgrade")
-print("All finished, your computer is now ready to roll! Taco Bell is da bomb!")
+updates = os.popen("sudo apt-get update && sudo apt-get -s dist-upgrade").read()
+print (updates)
+#updates = "Hello /n World"
+index1 = updates.find("upgraded,")
+index2 = index1
+while updates[index1] != "\n":
+	index1 = index1 -1
+print (updates [index1:index2])
+print (index1)
+print (index2)
+number = updates [index1:index2]
+numupdates = int(number.strip())
+print numupdates
+#os.system ("sudo apt-get dist-upgrade")
+print("All finished, your computer is now ready to roll!")
